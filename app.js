def1d1c7844806438c4c5ae8534979fe77c3efe6
summerhack <1256247113@qq.com>
2017-09-12 07:29:50 +0800

    submit  promise && socketio

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var http = require('http');
+var server = http.Server(app);
+var io = require('socket.io')(server);
+server.listen(80);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +29,88 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+var sendmy ='';
+// http.get('http://data.bter.com/api2/1/marketlist',(res) =>{
+//   res.setEncoding('utf8');
+//   let rawData = '';
+//   res.on('data', (chunk) => { rawData += chunk; });
+//   res.on('end', () => {
+//       const parsedData = JSON.parse(rawData);
+//       console.log(parsedData);
+//   });
+// })
+var coinData =function(callback){
+  //比特儿api
+  // let getBiter=function(){
+  //   return new Promise(function (resolve, reject) {
+  //     const biterOpts = {
+  //       hostname: 'data.bter.com',
+  //       path: '/api2/1/marketlist',
+  //       method: 'GET'
+  //     }
+  //     http.request(biterOpts, function(res) {
+  //       resolve(res)
+  //       console.log(res)
+  //       callback(res);
+  //     })
+  //   })
+  // }
+  // var p = new Promise(function (resolve, reject) {
+  //     resolve();
+  // });
+  // p.then(getBiter)
+  // 0.5秒后返回input*input的计算结果:
+  function multiply() {
+    return new Promise(function (resolve, reject) {
+      http.get('http://data.bter.com/api2/1/marketlist',(res) =>{
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+            const parsedData = JSON.parse(rawData);
+            sendmy = JSON.stringify(parsedData);
+            console.log(parsedData);
+        });
+      })
+      resolve(sendmy);
+    });
+  }
+  // 0.5秒后返回input+input的计算结果:
+  function add() {
+    return new Promise(function (resolve, reject) {
+      http.get('http://data.bter.com/api2/1/marketlist',(res) =>{
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+            const parsedData = JSON.parse(rawData);
+            sendmy = sendmy+JSON.stringify(parsedData);
+            console.log(parsedData);
+        });
+      })
+      resolve(sendmy);
+    });
+  }
 
+  var p = new Promise(function (resolve, reject) {
+    resolve();
+  });
+  p.then(multiply)
+  .then(function (result) {
+    callback(result);
+  });
+}
+io.on('connection', function (socket) {
+  var tweets = setInterval(function () {
+    coinData(function (msg) {
+      socket.volatile.emit('send data', msg);
+    });
+  }, 2000);
+
+  socket.on('disconnect', function () {
+    clearInterval(tweets);
+  });
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
